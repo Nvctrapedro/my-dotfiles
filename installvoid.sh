4b359eed10779ea8574ec01aa737cae259bdc7d0
Nvctrapedro <pedrosmalheiros10@gmail.com>
2026-06-10 11:09:07 -0300

    Create installvoid.sh

---
+++ installvoid.sh
@@ -0,0 +1,2 @@
+xbps-install -Su
+xbps-install wlroots0.19-devel libinput-devel wayland-devel libxkbcommon-devel wayland-protocols pkg-config libxcb-devel xcb-util-wm-devel xorg-server-xwayland fcft-devel pixman
